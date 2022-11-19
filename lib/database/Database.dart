2ef4be443544de3a3dbcce9cd73aa3698250d575
Goblin-hub <56274669+Goblin-hub@users.noreply.github.com>
2022-11-19 14:52:32 +0100

    Create Database.dart

---
+++ lib/database/Database.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+class Bird {
+  String name;
+  String latinName;
+  int temperature;
+  String soundPath;
+  DateTime date;
+
+  Bird(this.name, this.latinName, this.temperature, this.soundPath, this.date);
+
+  @override
+  String toString() => "Bird( name:$name; latinName:$latinName; temperature:$temperature; soundPath:$soundPath; date:$date)"; // Just for print()
+}
+
+void main() async {
+  // Register Adapter
+  Hive.registerAdapter(BirdAdapter()); 
+
+  var box = await Hive.openBox<List<Bird>>('birdsBox');
+
+  var now = DateTime.now();
+
+  addToKey(box, now, Bird('name 1', 'latin 1', 20, "path/sound.ogg", now));
+  addToKey(box, now, Bird('name 2', 'latin 2', 20, "path/sound.ogg", now));
+  var other = now.add(const Duration(days: 3, hours: 9));
+  addToKey(box, other, Bird('name 3', 'latin 3', 20, "path/sound.ogg", other));
+
+  var other2 = now.add(const Duration(days: 3, hours: 10));
+  addToKey(box, other2, Bird('name 4', 'latin 4', 20, "path/sound.ogg", other2));
+
+  debugPrint(box.toMap().entries.toString());
+}
+
+int storeDate(DateTime date) {
+  return (DateUtils.dateOnly(date).millisecondsSinceEpoch / 86400000).round();
+}
+
+void addToKey(Box<List<Bird>> box, DateTime date, Bird bird) {
+  int dateInt = storeDate(date);
+  if(box.containsKey(dateInt)) {
+    box.get(dateInt)!.add(bird);
+  
+  } else {
+    box.put(dateInt, List.filled(1, bird, growable: true));
+
+  }}
+
+// Can be generated automatically
+class BirdAdapter extends TypeAdapter<Bird> {
+  @override
+  final typeId = 0;
+
+  @override
+  Bird read(BinaryReader reader) {
+    return Bird(reader.readString(), reader.readString(), reader.readInt(), reader.readString(), DateTime.parse(reader.readString()));
+  }
+
+  @override
+  void write(BinaryWriter writer, Bird obj) {
+    writer.writeString(obj.name);
+    writer.writeString(obj.latinName);
+    writer.writeInt(obj.temperature);
+    writer.writeString(obj.soundPath);
+    writer.writeString(obj.date.toString());
+  }
+}
