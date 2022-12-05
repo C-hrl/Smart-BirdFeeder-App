0f8a2746bb15b481790120e780919f17cbea9fb1
Goblin-hub <56274669+Goblin-hub@users.noreply.github.com>
2022-12-05 19:10:04 +0100

    test dns discovery

--- lib/database/db.dart
+++ lib/database/db.dart
@@ -8,6 +8,7 @@ import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:smart_bird_feeder/database/network.dart';
 import 'package:smart_bird_feeder/main.dart';
+import 'package:nsd/nsd.dart';
 
 class Bird {
   String name;
@@ -93,6 +94,20 @@ Future<Box<List<Bird>>> setupDatabase() async {
       Bird('Rouge-Gorge', 'Erithacus rubecula', 10, 70.0, 50.4, "", other2));*/
 
   cachedDb = box;
+
+
+
+  final discovery = await startDiscovery('_http._tcp', ipLookupType: IpLookupType.any);
+  discovery.addListener(() {
+    for (var element in discovery.services) {
+      if(element.host == 'raspberry-piou.local') {
+        raspberryIp = '${element.addresses![0].address}:5000';
+        debugPrint('ip found : $raspberryIp');
+      }
+    }});
+  await stopDiscovery(discovery);
+  
+
   timedFetch = Timer.periodic(const Duration(seconds: 2), (timer) {
       getData('http://${raspberryIp.isNotEmpty ? raspberryIp: 'raspberry-piou.local:5000'}').then((birds) {
         debugPrint('fetch done');
